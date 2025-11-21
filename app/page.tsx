310d2c5ff3eaa17b76e2709e96c76e28a8acd837
shaykoteasital <shaykoteasital@gmail.com>
2025-11-22 02:30:39 +0600

    added news

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+'use client";'
 import Flipbook from "@/components/Flipbook";
-
+// import pdfPath from "../public/pdf/manifesto.pdf";
 export default function Home() {
   return (
     <div className="">
       <h1>Uwp Web</h1>
 
-      <Flipbook />
+      <Flipbook pdfPath={"/pdf/manifesto.pdf"} />
     </div>
   );
 }
--- components/Flipbook.tsx
+++ components/Flipbook.tsx
@@ -24,7 +24,7 @@ interface FlipbookProps {
 }
 
 export default function Flipbook({
-  pdfPath = '/pdf/manifesto.pdf',
+  pdfPath,
   pdfSize = '43 MB',
 }: FlipbookProps) {
   const containerRef = useRef<HTMLDivElement>(null)
